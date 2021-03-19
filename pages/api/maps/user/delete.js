68793a6073b542836c85c4912fa70a2f8ce31e14
JEbertPrime <uhsalad@gmail.com>
2021-03-19 10:50:40 -0400

    started api work
    Added (untested) api routes for delete, get, update user map

---
+++ pages/api/maps/user/delete.js
@@ -0,0 +1,38 @@
+import connectDB from '../../../../middleware/mongodb.js';
+import Map from '../../../../models/map.js';
+import { getSession } from 'next-auth/client'
+
+const handler = async (req, res) => {
+    const session = await getSession({ req })
+    
+
+    if(session){
+        if (req.method === 'POST') {
+            // Check if name, email or password is provided
+            const { mapId } = JSON.parse(req.body);
+            if(user == session.user.id){
+            if (user) {
+                try {
+                
+                //Find single map by id
+                Map.deleteOne({_id: mapId}, function (err){
+                    res.status(500).send(err.message)
+                })
+                return res.status(200).send('deleted');
+                } catch (error) {
+                return res.status(500).send(error.message);
+                }
+            } else {
+                res.status(422).send('data_incomplete');
+            }
+        }else{
+            res.status(401).send('User id mismatch')
+        }
+        } else {
+            res.status(422).send('req_method_not_supported');
+        }
+    
+    }
+};
+
+export default connectDB(handler);
